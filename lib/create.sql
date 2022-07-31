7ba9ecc12768e2b3b047643557cd402f091498c8
Joshua Welsh <j22yosh@gmail.com>
2022-07-31 08:32:15 -0400

    completed assignment

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+create TABLE
+  bears(
+    id INTEGER PRIMARY KEY,
+    name text,
+    age INTEGER,
+    sex text,
+    color text,
+    temperament text,
+    alive boolean
+  );
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Mr. Chocolate", 22, "M", "Black", "Chocolatey", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Rowdy", 13, "M", "Black", "Rowdy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Tabitha", 6, "F", "Black", "Happy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Sergeant Brown", 37, "M", "Brown", "Disciplined", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Melissa", 19, "F", "White", "Depressed", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Grinch", 22, "M", "Black", "Angry", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Wendy", 22, "F", "Black", "Happy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(NULL, NULL, "M", "Orange", "Dead", 0);
